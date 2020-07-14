6daa72384bc120d71501095c0c7e553b2cf6a5cd
ShimiShimson <szymon.jakub.adamczuk@gmail.com>
2020-07-14 15:03:12 +0200

    -routing table created -starting page is LoginPageComponent -router-outlet added to main html template

--- Project-Timer-frontend/src/app/app-routing.module.ts
+++ Project-Timer-frontend/src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginPageComponent } from './login-page/login-page.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/login-page', pathMatch: 'full' },
+  { path: 'login-page', component: LoginPageComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
