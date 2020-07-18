eee1edb1940ceeea397c5255a18cdaa24828244a
ShimiShimson <szymon.jakub.adamczuk@gmail.com>
2020-07-18 14:45:46 +0200

    Unneeded code deleted

--- Project-Timer-frontend/src/app/login-page/login-page.component.ts
+++ Project-Timer-frontend/src/app/login-page/login-page.component.ts
@@ -1,15 +1,12 @@
-import {Component, OnInit} from '@angular/core';
-import {AbstractControl, FormControl, Validators} from '@angular/forms';
+import {Component} from '@angular/core';
+import {FormControl, Validators} from '@angular/forms';
 
-/**
- * @title Input with error messages
- */
 @Component({
   selector: 'app-login-page',
   templateUrl: './login-page.component.html',
   styleUrls: ['./login-page.component.scss'],
 })
-export class LoginPageComponent implements OnInit {
+export class LoginPageComponent {
 
   loginSuccess = false;
   formSubmitted = false;
@@ -26,11 +23,6 @@ export class LoginPageComponent implements OnInit {
   ])
 
   onSubmit() {
-    console.log(this.emailFormControl)
-    console.log(this.passwordFormControl)
     this.loginSuccess = true;
-  }
-
-  ngOnInit () {
   }
 }
