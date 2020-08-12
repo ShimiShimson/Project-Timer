e8c810b525de6a11cec68e4c62b2a218471297ca
ShimiShimson <szymon.jakub.adamczuk@gmail.com>
2020-08-12 18:21:21 +0200

    User is taken to dashboard after success login

--- Project-Timer-frontend/src/app/app-routing.module.ts
+++ Project-Timer-frontend/src/app/app-routing.module.ts
@@ -1,12 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
 import { LoginPageComponent } from './login-page/login-page.component';
 
 const routes: Routes = [
-  {
-    path: '',
-    component: LoginPageComponent
-  }
+  { path: '', component: LoginPageComponent },
+  { path: 'dashboard', component: DashboardComponent },
 ];
 
 @NgModule({
--- Project-Timer-frontend/src/app/login-page/login-page.component.ts
+++ Project-Timer-frontend/src/app/login-page/login-page.component.ts
@@ -35,6 +35,6 @@ export class LoginPageComponent implements OnInit{
   }
 
   onSubmit(): void {
-    this.router.navigate([''])
+    this.router.navigate(['/dashboard'])
   }
 }
