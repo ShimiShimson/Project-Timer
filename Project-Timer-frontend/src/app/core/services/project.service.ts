7ae233d6f8c34c8826a52f0fa8f99e467cb633b2
ShimiShimson <szymon.jakub.adamczuk@gmail.com>
2020-08-19 20:42:49 +0200

    add-project functionality only

--- Project-Timer-frontend/src/app/core/services/project.service.ts
+++ Project-Timer-frontend/src/app/core/services/project.service.ts
@@ -8,40 +8,9 @@ import { Project } from '../../shared/interfaces/project.interface';
 
 export class ProjectService {
   private projects: Project[] = [];
-  private projects$ = new BehaviorSubject<Project[]>(this.projects);
 
   public addProject(project: Project): void {
     this.projects.push(project);
-    if (this.isLocalEmpty()) {
-      this.ifLocalEmpty(project);
-    } else {
-      this.ifLocalHaveProjects(project);
-    }
-  }
-
-  private isLocalEmpty (): boolean {
-    if(!JSON.parse(localStorage.getItem("projectList"))) return true
-    return false;
-  }
-
-  private ifLocalHaveProjects (project: Project) {
-      const jsonData = JSON.parse(localStorage.getItem("projectList"));
-      const projectList: Project[] = jsonData;
-      projectList.push(project);
-      localStorage.setItem("projectList", JSON.stringify(projectList));
-  }
-
-  private ifLocalEmpty (project: Project) {
-    const projectList = [];
-    projectList.push(project);
-    localStorage.setItem("projectList", JSON.stringify(projectList));
-  }
-
-  public getProjects(): BehaviorSubject<Project[]> {
-    if (!this.isLocalEmpty()) {
-      const jsonData = JSON.parse(localStorage.getItem("projectList"));
-      this.projects.push(...jsonData);
-    }
-    return this.projects$;
+    console.log(this.projects);
   }
 }
